303b6335c2db53c94fd414f33d00b5b3c9d94ede
David <dhoesen@gmail.com>
2024-04-15 14:47:07 +0200

    Fat Arrow

---
+++ 04 Funkcja Strzałkowa/script.js
@@ -0,0 +1,12 @@
+const multiply = (a,b) => a*b;
+console.log(multiply(2,10));
+
+const substract = (a,b) => {
+    return a-b
+}
+
+console.log(substract(20,4));
+
+let show = (...args) => console.log(args);
+show(1,2,3,4,5,6,7)
+ 
