aa7ca2149818f7e6639e08cb87b9b7e4358fe402
David <dhoesen@gmail.com>
2024-04-18 14:03:40 +0200

    60%

---
+++ 09 Window object/script1.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function (){
+    console.log("Website downloaded and parsed!");
+});
+window.addEventListener("resize", () => {
+    console.log(window.innerHeight, window.innerWidth);
+    
+})
